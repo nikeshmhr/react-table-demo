ba6e5b9bf82efd7e2171409f2c443fb28325af53
Nikesh Maharjan <nikeshmhr@gmail.com>
2020-10-06 15:29:41 +0545

    Working stuff.

--- src/App.js
+++ src/App.js
@@ -1,127 +1,139 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { useTable } from 'react-table'
 
 import makeData from './makeData'
 
 const Styles = styled.div`
-  padding: 1rem;
-
+    position: relative;
+    width:100%;
+    z-index: 1;
+    margin: auto;
+    overflow: auto;
+    height: 700px;
+ 
   table {
-    border-spacing: 0;
-    border: 1px solid black;
-
-    tr {
-      :last-child {
-        td {
-          border-bottom: 0;
-        }
-      }
+      width: 100%;
+      min-width: 1280px;
+      margin: auto;
+      border-collapse: separate;
+      border-spacing: 0;
     }
+.table-wrap {
+  position: relative;
+}
 
-    th,
-    td {
-      margin: 0;
-      padding: 0.5rem;
-      border-bottom: 1px solid black;
-      border-right: 1px solid black;
+thead th {
+  background: #333;
+  color: #fff;
+  position: -webkit-sticky;
+  position: sticky;
+  top: 0;
+}
 
-      :last-child {
-        border-right: 0;
-      }
-    }
+tfoot,
+tfoot th,
+tfoot td {
+  position: -webkit-sticky;
+  position: sticky;
+  bottom: 0;
+  background: #666;
+  color: #fff;
+  z-index: 4;
   }
+  
+  th:first-child {
+  position: -webkit-sticky;
+  position: sticky;
+  left: 0;
+  z-index: 2;
+  background: #ccc;
+}
+thead th:first-child,
+tfoot th:first-child {
+  z-index: 5;
+}
 `
 
-function Table({ columns, data }) {
-  // Use the state and functions returned from useTable to build your UI
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-  } = useTable({
-    columns,
-    data,
-  })
 
-  // Render the UI for your table
-  return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+function Table({ columns, data }) {
+    // Use the state and functions returned from useTable to build your UI
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+    } = useTable(
+        {
+            columns,
+            data
+        });
+
+// Render the UI for your table
+    return (
+        <table {...getTableProps()}>
+            <thead>
+            {headerGroups.map(headerGroup => (
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                    {headerGroup.headers.map(column => (
+                        <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                    ))}
+                </tr>
             ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row)
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map(cell => {
-                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-              })}
-            </tr>
-          )
-        })}
-      </tbody>
-    </table>
-  )
+            </thead>
+            <tbody {...getTableBodyProps()}>
+            {rows.map((row, i) => {
+                prepareRow(row)
+                return (
+                    <tr {...row.getRowProps()}>
+                        {row.cells.map((cell, index) => {
+                            console.log('cell', cell);
+                            if(index === 0) {
+                                return <th {...cell.getCellProps()}>{cell.render('Cell')}</th>;
+                            } else {
+                                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                            }
+                        })}
+                    </tr>
+                )
+            })}
+            </tbody>
+        </table>
+    )
 }
 
 function App() {
-  const columns = React.useMemo(
-    () => [
-      {
-        Header: 'Name',
-        columns: [
-          {
-            Header: 'First Name',
-            accessor: 'firstName',
-          },
-          {
-            Header: 'Last Name',
-            accessor: 'lastName',
-          },
-        ],
-      },
-      {
-        Header: 'Info',
-        columns: [
-          {
-            Header: 'Age',
-            accessor: 'age',
-          },
-          {
-            Header: 'Visits',
-            accessor: 'visits',
-          },
-          {
-            Header: 'Status',
-            accessor: 'status',
-          },
-          {
-            Header: 'Profile Progress',
-            accessor: 'progress',
-          },
-        ],
-      },
-    ],
-    []
-  )
+    const [columnCount, setColumnCount] = useState(7);
+    const columns = React.useMemo(
+        () => {
+            return Array(columnCount).fill(0).map((item, index) => {
+                return {
+                    Header: `Column ${index + 1}`,
+                    accessor: `column${index}`,
+                    sticky: 'top',
+                }
+            })
+        },
+        [columnCount]
+    );
 
-  const data = React.useMemo(() => makeData(20), [])
+    const handleAddColumn = () => {
+        setColumnCount(columnCount + Math.trunc(Math.random() * 10))
+    }
 
-  return (
-    <Styles>
-      <Table columns={columns} data={data} />
-    </Styles>
-  )
+    const data = React.useMemo(() => makeData(100, columnCount), [columnCount])
+
+    console.log(columns, data);
+
+    return (
+        <>
+            <button type="button" onClick={handleAddColumn} style={{ margin: '10px' }}>AddColumns {columnCount}</button>
+            <Styles>
+                <Table columns={columns} data={data}/>
+            </Styles>
+        </>
+    )
 }
 
 export default App
--- src/makeData.js
+++ src/makeData.js
@@ -1,40 +1,31 @@
 import namor from 'namor'
 
 const range = len => {
-  const arr = []
-  for (let i = 0; i < len; i++) {
-    arr.push(i)
-  }
-  return arr
+    const arr = []
+    for(let i = 0; i < len; i++) {
+        arr.push(i)
+    }
+    return arr
 }
 
-const newPerson = () => {
-  const statusChance = Math.random()
-  return {
-    firstName: namor.generate({ words: 1, numbers: 0 }),
-    lastName: namor.generate({ words: 1, numbers: 0 }),
-    age: Math.floor(Math.random() * 30),
-    visits: Math.floor(Math.random() * 100),
-    progress: Math.floor(Math.random() * 100),
-    status:
-      statusChance > 0.66
-        ? 'relationship'
-        : statusChance > 0.33
-        ? 'complicated'
-        : 'single',
-  }
+const newPerson = (columns) => {
+    let result = {};
+    range(columns).forEach((item, index) => {
+        result[`column${index}`] = namor.generate({ words: 1, numbers: 0 })
+        result['id'] = index;
+    });
+    return result;
 }
 
 export default function makeData(...lens) {
-  const makeDataLevel = (depth = 0) => {
-    const len = lens[depth]
-    return range(len).map(d => {
-      return {
-        ...newPerson(),
-        subRows: lens[depth + 1] ? makeDataLevel(depth + 1) : undefined,
-      }
-    })
-  }
+    const makeDataLevel = (depth = 0) => {
+        const len = lens[depth]
+        return range(len).map(d => {
+            return {
+                ...newPerson(lens[1]),
+            }
+        })
+    }
 
-  return makeDataLevel()
+    return makeDataLevel()
 }
